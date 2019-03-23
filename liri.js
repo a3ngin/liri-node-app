7d0f2a811871741676a52de723455de0fd263b17
Anne Engin <anne@Annes-MacBook-Air.local>
2019-03-23 15:08:05 -0400

    spotifystart

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var keys = require("./keys.js");
 var Spotify =  require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var axios =  require("axios");
+var moment = require("moment");
 
 var input = process.argv;
 var request =  input[2];
@@ -19,6 +20,13 @@ function start() {
         concert();
         break;
 
+        case "spotify-this-song":
+        music();
+        break;
+
+
+
+
         default:
         console.log("Invalid");
     }
@@ -27,6 +35,20 @@ function start() {
 start();
 
 function concert() {
-    axios.get("https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp").then
-
+    axios.get("https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp").then(function(response){
+        for(i=0; i <response.data.length; i++){
+        console.log("Name of Venue: " + response.data[i].venue.name);
+        console.log("Location of Venue: " + response.data[i].venue.city + ", " + response.data[0].venue.region);
+        console.log("Event Date: " + moment(response.data[i].datetime).format("L"));
+        }
+    })
+}
+function music(){
+    spotify.search({ type: 'track', query: info, limit: 1 }).then(function(response) {
+        console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
+        console.log("The song's name: " + response.tracks.items[0].name);
+        console.log("A preview link of the song from Spotify: " + response.tracks.items[0].preview_url);
+        console.log("The album that the song is from: " + response.tracks.items[0].album.name);
+    //   console.log(response); 
+      });
 }
